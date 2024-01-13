31c0a8da3977a932065b82db92fb9dc0c62690c7
ppratham0811 <ppratham0811@gmail.com>
2024-01-13 18:21:53 +0530

    Pool contract and Vault Contract

---
+++ smart-contract/contracts/Pool.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// │      PoolAddressesProvider-Polygon      │ '0x4CeDCB57Af02293231BAA9D39354D6BFDFD251e0' │
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+// import "https://github.com/ppratham0811/gho-core/blob/main/src/contracts/facilitators/aave/tokens/GhoAToken.sol";
+import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Pool {
+    IERC20 private GhoTokenAddress =
+        IERC20(0xc4bF5CbDaBE595361438F8c6a187bDc330539c60);
+
+    IPool public immutable POOL;
+
+    IPoolAddressesProvider public immutable ADDRESSES_PROVIDER;
+
+    constructor() {
+        ADDRESSES_PROVIDER = IPoolAddressesProvider(
+            0x4CeDCB57Af02293231BAA9D39354D6BFDFD251e0
+        );
+        POOL = IPool(ADDRESSES_PROVIDER.getPool());
+    }
+
+    function createSupply(address _tokenAddress, uint256 _amount) external {
+        address asset = _tokenAddress;
+        uint256 amount = _amount;
+        address onBehalfOf = address(this);
+        uint16 referralCode = 0;
+        POOL.supply(asset, amount, onBehalfOf, referralCode);
+    }
+
+    function withdrawFromSupply(
+        address _tokenAddress,
+        uint256 _amount
+    ) external returns (uint256) {
+        address asset = _tokenAddress;
+        uint256 amount = _amount;
+        address to = address(this);
+        return POOL.withdraw(asset, amount, to);
+    }
+}
--- smart-contract/contracts/Vault.sol
+++ smart-contract/contracts/Vault.sol
@@ -1,17 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+// │      PoolAddressesProvider-Polygon      │ '0x4CeDCB57Af02293231BAA9D39354D6BFDFD251e0' │
+
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+// import "https://github.com/ppratham0811/gho-core/blob/main/src/contracts/facilitators/aave/tokens/GhoAToken.sol";
+import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LFGHOVault {
-    constructor() {
-        console.log("Deploying a Vault");
-    }
+    // IPool public immutable POOL = IPool();
 
-    function add() private returns (uint256) {
-        return false;
+    constructor() {}
+
+    function stakeAssets(address _tokenAddress, uint256 _amount) external {
+        // fn which will add money to the liquidity pool and give GHO Token to the msg.sender guy
+        address asset = _tokenAddress;
+        uint256 amount = _amount;
+        address onBehalfOf = msg.sender;
+        uint16 referralCode = 0;
+        // POOL.deposit(asset, amount, onBehalfOf, referralCode);
     }
 }
