e1c16daf3f263ca06250c7dc5cdfc9397d52f44e
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-01-13 15:12:15 +0530

    added hardhat

---
+++ smart-contract/contracts/Vault.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+contract LFGHOVault {
+    constructor() {
+        console.log("Deploying a Vault");
+    }
+
+    function add() private returns (uint256) {
+        return false;
+    }
+}
