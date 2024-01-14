5f6ebe2046455bbac8f53e15d8ae7fbf95e67c48
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-01-14 21:28:35 +0530

    contract instantiated

--- smart-contract/contracts/Vault.sol
+++ smart-contract/contracts/Vault.sol
@@ -1,27 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-// │      PoolAddressesProvider-Polygon      │ '0x4CeDCB57Af02293231BAA9D39354D6BFDFD251e0' │
+pragma solidity 0.8.10;
 
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-// import "https://github.com/ppratham0811/gho-core/blob/main/src/contracts/facilitators/aave/tokens/GhoAToken.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../gho-contract/contracts/facilitators/aave/tokens/GhoAToken.sol";
 
-contract LFGHOVault {
-    // IPool public immutable POOL = IPool();
+contract LFGHOVault is ERC4626, IERC4626 {
+    IPool public immutable POOL;
+    IPoolAddressesProvider public immutable ADDRESSES_PROVIDER;
+    address payable owner;
 
-    constructor() {}
+    constructor(
+        address _addressProvider,
+        IERC20 _asset
+    ) ERC4626(_asset) ERC20("LFGHOCoin", "LFCOIN") {
+        ADDRESSES_PROVIDER = IPoolAddressesProvider(_addressProvider);
+        POOL = IPool(ADDRESSES_PROVIDER.getPool());
+        owner = payable(msg.sender);
+    }
 
-    function stakeAssets(address _tokenAddress, uint256 _amount) external {
+    function stakeMoneyToVault(address _token, uint256 _amount) external {}
+
+    function stakeMoneyinLiquidityPool(
+        address _tokenAddress,
+        uint256 _amount
+    ) external {
         // fn which will add money to the liquidity pool and give GHO Token to the msg.sender guy
         address asset = _tokenAddress;
         uint256 amount = _amount;
         address onBehalfOf = msg.sender;
         uint16 referralCode = 0;
-        // POOL.deposit(asset, amount, onBehalfOf, referralCode);
+        POOL.deposit(asset, amount, onBehalfOf, referralCode);
     }
 }
--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "^0.8.10",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 150,
+          }
+        }
+      },
+
+      {
+        version: "0.8.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 150,
+          },
+        }
+      },
+    ]
+  },
+  networks: {
+    forking: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.API_KEY}`,
+    }
+  }
 };
