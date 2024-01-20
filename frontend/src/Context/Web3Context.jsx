32f11ae7674753462a7254bd1606c9031fce742d
Fidal <fidal15perfect@gmail.com>
2024-01-20 14:22:30 +0530

    added some stuff in context

--- frontend/src/Context/Web3Context.jsx
+++ frontend/src/Context/Web3Context.jsx
@@ -1,9 +1,12 @@
-import {createContext} from "react";
-
+import { createContext } from "react";
+// import abi from '../lib/abi.json';
 export const Web3Context = createContext();
 
+// const contractAddress = "";
+const [contractAddress, setContractAddress] = useState("0xd49A69020D7DDA345aC0df4713193CfDB0b731D9")
+
 const Web3ContextProvider = () => {
-  <Web3Context.Provider value={{}}></Web3Context.Provider>;
+  <Web3Context.Provider value={{ contractAddress, setContractAddress }}></Web3Context.Provider>;
 };
 
 export default Web3ContextProvider;
