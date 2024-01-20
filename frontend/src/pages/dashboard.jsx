cabe50399149fe3a865edf855e72eb9b5fdd1635
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-01-20 14:32:31 +0530

    added modal

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,7 +1,7 @@
-import { ModeToggle } from "@/components/Toggletheme";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import {ModeToggle} from "@/components/Toggletheme";
+import {Button} from "@/components/ui/button";
+import {useRouter} from "next/navigation";
+import {useEffect, useState} from "react";
 import {
   Table,
   TableBody,
@@ -12,14 +12,14 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
-import { disconnect } from "@wagmi/core";
+import {useAccount, useNetwork, useSwitchNetwork} from "wagmi";
+import {disconnect} from "@wagmi/core";
 
-import { toast } from "@/components/ui/use-toast";
-import { ConnectKitButton } from "connectkit";
-import { useTheme } from "next-themes";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {toast} from "@/components/ui/use-toast";
+import {ConnectKitButton} from "connectkit";
+import {useTheme} from "next-themes";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import {
   Select,
   SelectContent,
@@ -35,24 +35,24 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import * as Yup from "yup";
 
 export default function Dashboard() {
-  const { isConnected, address } = useAccount();
-  const { chain } = useNetwork();
+  const {isConnected, address} = useAccount();
+  const {chain} = useNetwork();
   const [mounted, setMounted] = useState(false);
   const router = useRouter();
-  const { chains, switchNetwork } = useSwitchNetwork();
+  const {chains, switchNetwork} = useSwitchNetwork();
   console.log("chains switch", chain);
 
   // open all modals
   const [openStakingModal, setOpenStakingModal] = useState(false);
   const [openWithdrawModal, setOpenWithdrawModal] = useState(false);
   const [sendGHOModal, setSendGHOModal] = useState(false);
-
-
-
-
-
+  const [addMemberModal, setAddMemberModal] = useState(false);
 
   useEffect(() => {
     if (!isConnected) {
@@ -79,23 +79,6 @@ export default function Dashboard() {
 
   useEffect(() => setMounted(true), []);
   if (!mounted) return null;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return (
     <div className="h-screen flex items-center flex-col">
@@ -262,15 +245,69 @@ export default function Dashboard() {
 
       <div
         className="grid h-screen grid-flow-row grid-cols-1 gap-4 p-8 xl:grid-cols-3 xl:grid-rows-5 w-full"
-      // style={{gridTemplateColumns: `repeat(auto-fit, minmax(400px, 1fr))`}}
+        // style={{gridTemplateColumns: `repeat(auto-fit, minmax(400px, 1fr))`}}
       >
+        <Dialog open={addMemberModal} onOpenChange={setAddMemberModal}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add New Member</DialogTitle>
+              <DialogDescription className="h-fit">
+                <Formik
+                  initialValues={{role: "", address: ""}}
+                  onSubmit={(values, _) => console.log(values)}
+                >
+                  {(formik) => (
+                    <Form className="py-5 flex flex-col space-y-6">
+                      <div className="flex">
+                        <Label htmlFor="address" className="w-[100px] my-auto">
+                          Address:{" "}
+                        </Label>
+                        <Field
+                          as={Input}
+                          name="address"
+                          type="text"
+                          id="address"
+                          placeholder="0x45fef..."
+                          className="flex-1"
+                        />
+                        <ErrorMessage name="address" />
+                      </div>
+                      <div className="flex">
+                        <Label htmlFor="role" className="w-[100px] my-auto">
+                          Role:{" "}
+                        </Label>
+                        <Select
+                          onValueChange={(val) => {
+                            formik.setFieldValue("role", val);
+                          }}
+                        >
+                          <SelectTrigger className="flex-1">
+                            <SelectValue placeholder="Enter Role" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="facilitator">
+                              Facilitator
+                            </SelectItem>
+                            <SelectItem value="member">Member</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <ErrorMessage name="role" />
+                      </div>
+                      <Button type="submit">Submit</Button>
+                    </Form>
+                  )}
+                </Formik>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
         <Card className="h-full w-full rounded-xl row-span-full">
           <CardHeader className="flex flex-row items-center justify-between mt-1">
             <CardTitle className="">All Members</CardTitle>
             <Button
               className="m-0 w-fit"
               size={"sm"}
-            // onClick={() => setOpenNewAppointment((prev) => !prev)}
+              onClick={() => setAddMemberModal((prev) => !prev)}
             >
               Add New Member
             </Button>
